b62177f1c20c319df374d56b636d7bc25ee48b46
Arush Sharma <arushmohansharma@gmail.com>
2022-10-16 17:06:56 +0530

    add kafka, redis, zookeeper components

---
+++ redis/redisadapter.py
@@ -0,0 +1,36 @@
+import redis
+
+# r=redis.Redis(
+#     host='0.0.0.0',
+#     port=6379
+# )
+
+# example of using adapter
+# r.set('foo', 'bar')
+# value = r.get('foo')
+# print(value)
+
+# Helper class around redis
+
+
+class RedisBaseAdapter:
+    def __init__(self, host, port) -> None:
+        self.host = host
+        self.port = port
+        self.client = None
+        self._initialize_client()
+
+    def _initialize_client(self):
+        self.client = redis.Redis(
+            host='0.0.0.0',
+            port=6379
+        )
+
+    def get(self, key):
+        return self.client.get(key)
+
+    def set(self, key, value):
+        return self.client.set(key, value)
+    
+
+    
